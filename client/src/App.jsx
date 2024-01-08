eaea824e937d94fa935212aa4b4ecd383d8e45f2
shankaravi6 <shankaravi6.india@gmail.com>
2024-01-08 20:57:15 +0530

    check changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
   return (
     <div className='home-page'>
       <h1 className='home-title'>SILO</h1>
-      <p className='home-description pb-7'>The Truth Will Surface</p>
+      <p className='home-description pb-7'>The Truth Will Surface...</p>
       <TextField className='mb-5 home-input' color="warning" value={email} label='Email' onChange={(e) => setEmail(e.target.value)} variant="filled"/><br/>
       <Button className='home-btn' variant='contained' onClick={handleSubmit}  color='warning'>JOIN</Button>
     </div>
