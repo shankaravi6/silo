41cbde08a964084012081849377ac4093854e82f
Shankar <69907363+shankaravi6@users.noreply.github.com>
2024-05-06 07:23:37 +0530

    Delete client/src/ThemeProvider directory

--- client/src/ThemeProvider/ThemeProvider.jsx
+++
@@ -1,9 +0,0 @@
-import React, { createContext, useContext } from "react";
-
-const ThemeContext = createContext();
-
-export const useThemeContext = () => useContext(ThemeContext);
-
-export const ThemeProvider = ({ children, theme }) => (
-  <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
-);
