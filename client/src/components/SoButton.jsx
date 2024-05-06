33a536971855f70e1160362bd392d7e3dfa2bcb4
shankaravi6 <shankaravi6.india@gmail.com>
2024-05-06 07:28:30 +0530

    first commit

---
+++ client/src/components/SoButton.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
+import { useThemeContext } from "../themeprovider/ThemeProvider";
+
+const mapStateToProps = (state) => ({
+    mode: state.silo.mode,
+  });
+  
+  const SiloComponent = (StyledComponent) => {
+    const ConnectedStyledComponent = connect(mapStateToProps)(
+      ({ dispatch, ...rest }) => {
+        const { palette } = useThemeContext();
+        return <StyledComponent {...rest} palette={palette} />;
+      }
+    );
+  
+    return ConnectedStyledComponent;
+  };
+
+const StyledButton = SiloComponent(styled.button`
+  font-size:100%;
+  text-align: center;
+  letter-spacing: 5px;
+  color: ${(props) => props.palette.text.high};
+  background-color: ${(props) => props.palette.background.low};
+  box-shadow: ${(props) => props.palette.shadow.main} 0px 2px 4px;
+  transition: all 0.3s ease;
+  font-weight: 500;
+  padding: 10px 20px;
+  width: ${(props) => props.width ? props.width : '100%'};
+  border-radius: 5px;
+
+  &:hover {
+  box-shadow: ${(props) => props.palette.shadow.main} 0px 5px 15px;
+  background-color: ${(props) => props.palette.background.mid};
+  }
+`);
+
+
+const SoButton = ({ height, width, onChange, onSubmit, onClick, onBlur, type, children, style }) => {
+  return (
+      <StyledButton
+        height={height}
+        width={width}
+        onChange={onChange}
+        onSubmit={onSubmit}
+        onClick={onClick}
+        onBlur={onBlur}
+        type={type}
+        style={style}
+      >
+      {children}
+      </StyledButton>
+  );
+};
+
+export default SoButton;
---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { setMode } from "../state";
+import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import SoButton from "../Components/SoButton";
+import { SoContainer, SoFlex, SoSection, SoSubTitle, SoTitle} from "../styledcomponents/globalStyles";
+import { useThemeContext } from "../themeprovider/ThemeProvider";
+
+
+
+const LandingPage = () => {
+ 
+  const { palette } = useThemeContext();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  return (
+    <SoContainer className='flex justify-center items-center'>
+      <SoSection>
+        <SoFlex dir='column'>
+        <SoTitle fs='clamp(5rem, 15vw, 10rem)' ls='50px'>
+         SILO
+       </SoTitle>
+       <SoSubTitle
+         className="pb-7"
+       >
+         The Truth Will Surface
+       </SoSubTitle>
+       <SoButton width='auto' onClick={()=> navigate('/join')}>
+         Join
+       </SoButton>
+       </SoFlex>
+      </SoSection>
+    </SoContainer>
+  );
+};
+
+export default LandingPage;
